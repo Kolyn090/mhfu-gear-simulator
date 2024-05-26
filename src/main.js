7fb099aa4be6e66f478478b84b2a5ef6f8ca65ed
Kolyn090 <78561131+Kolyn090@users.noreply.github.com>
2024-05-25 21:26:10 -0400

    Added a function to brute force all decorated armors

--- src/main.js
+++ src/main.js
@@ -1,22 +1,20 @@
+const getter = require('./getter');
+const get_valid_armors = getter.get_valid_armors;
+const get_valid_decorations = getter.get_valid_decorations;
+const get_required_skills = getter.get_required_skills;
+const discard_outclassed_armors = getter.discard_outclassed_armors;
+const insert_decorations = require('./decoration_inserter').insert_decorations;
 
-// const skill_names = ["Health -30"];
-// const skills = get_required_skills(skill_names);
-// console.log(get_valid_armors(skills).length);
-// console.log(discard_outclassed_armors(get_valid_armors(skills), skills));
+const brute_force = () => {
+    const skill_names = ["High Grade Earplug", "Evade +2", "Health -30"];
+    const required_skills = get_required_skills(skill_names);
+    const valid_armors = get_valid_armors(required_skills);
+    const valid_decorations = get_valid_decorations(required_skills);
+    const optimal_armors = discard_outclassed_armors(valid_armors, required_skills);
+    const decorated_armors = optimal_armors.map(armor => insert_decorations(armor, valid_decorations)).flat();
+    for (let i = 0; i < decorated_armors.length; i++) {
+        console.log(decorated_armors[i]);
+    }
+};
 
-// const skill_names = ["Attack Up (Small)"];
-// const skills = get_required_skills(skill_names);
-// console.log(get_valid_armors(skills)[10]);
-// console.log(get_complete_decorations(get_valid_armors(skills)[10], get_valid_decorations(skills)))
-
-// const skill_names = ["Attack Up (Small)"];
-// const skills = get_required_skills(skill_names);
-// const valid_armors = get_valid_armors(skills);
-// const valid_decorations = get_valid_decorations(skills);
-// console.log(make_decorated_armor(valid_armors[10], valid_decorations));
-
-// const skill_names = ["Attack Up (Small)"];
-// const skills = get_required_skills(skill_names);
-// const valid_armors = get_valid_armors(skills);
-// const valid_decorations = get_valid_decorations(skills);
-// console.log(insert_decorations(valid_armors[10], valid_decorations));
+brute_force();
