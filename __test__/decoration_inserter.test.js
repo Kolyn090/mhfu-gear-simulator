ab947df6c27bd88b58b82c5a11b0c8cfe7a18285
Kolyn090 <78561131+Kolyn090@users.noreply.github.com>
2024-05-25 21:23:50 -0400

    Added more tests

--- __test__/decoration_inserter.test.js
+++ __test__/decoration_inserter.test.js
@@ -1,6 +1,55 @@
 const insert_decorations = require('../src/decoration_inserter').insert_decorations;
 
-const test_armor = {
+const test_armor2 = {
+    "id": 3853,
+    "part": "waist",
+    "name": "WhiteFatalisScaleZ",
+    "rare": 10,
+    "price": 66666,
+    "hunter-type": "G",
+    "defense": 54,
+    "skill-points": [
+        {
+            "name": "Edgemaster",
+            "points": 2
+        },
+        {
+            "name": "Rec Speed",
+            "points": 4
+        },
+        {
+            "name": "Fate",
+            "points": -3
+        }
+    ],
+    "description": "",
+    "slots": 2,
+    "fire-res": 0,
+    "thundr-res": 2,
+    "dragon-res": -5,
+    "water-res": 2,
+    "ice-res": 2,
+    "materials": [
+        {
+            "name": "HvyWhtFatalisShl",
+            "amount": 3
+        },
+        {
+            "name": "ThckWhtFatlisScl",
+            "amount": 2
+        },
+        {
+            "name": "StrWhtFatalisWng",
+            "amount": 1
+        },
+        {
+            "name": "BigEDragonJewel",
+            "amount": 1
+        }
+    ]
+};
+
+const test_armor3 = {
     "id": 3852,
     "part": "gauntlet",
     "name": "White Fatalis Fist Z",
@@ -153,7 +202,7 @@ const expecting_points = (skill_points, skill_name, expect_val) => {
 }
 
 it('insertes three same slot-1 decoration on a slot-3 armor', () => {
-    const insert1 = insert_decorations(test_armor, [test_decoration1]);
+    const insert1 = insert_decorations(test_armor3, [test_decoration1]);
     expect(insert1[0]["armor-id"]).toBe(3852);
     expect(insert1[0]["decoration-ids"][0]).toBe(0);
     
@@ -164,3 +213,36 @@ it('insertes three same slot-1 decoration on a slot-3 armor', () => {
     expecting_points(skill_points, "Attack", 3);
 });
 
+it('insertes one slot-3 decoration on a slot-3 armor', () => {
+    const insert3 = insert_decorations(test_armor3, [test_decoration3]);
+    expect(insert3[0]["armor-id"]).toBe(3852);
+    expect(insert3[0]["decoration-ids"][0]).toBe(2);
+    
+    const skill_points = insert3[0]["skill-points"];
+    expecting_points(skill_points, "SteadyHand", 2);
+    expecting_points(skill_points, "Fate", -3);
+    expecting_points(skill_points, "Constitutn", 2);
+    expecting_points(skill_points, "Attack", 5);
+});
+
+it('insertes one slot-1 and one slot-2 decorations on a slot-3 armor', () => {
+    const insert12 = insert_decorations(test_armor3, [test_decoration1, test_decoration2]);
+    expect(insert12[0]["armor-id"]).toBe(3852);
+
+    const dec_ids = insert12[0]["decoration-ids"];
+    const slot2_and_slot1 = 
+        dec_ids.length === 2 &&
+        ((dec_ids[0] === 0 && dec_ids[1] === 1) ||
+        (dec_ids[0] === 1 && dec_ids[1] === 0));
+    const three_slot1 = 
+        dec_ids.length === 3 &&
+        dec_ids[0] === 0 && 
+        dec_ids[1] === 0 && 
+        dec_ids[2] === 0;
+    expect(slot2_and_slot1 || three_slot1).toBe(true);
+});
+
+it('cannot insert a slot-3 decoration on a slot-2 armor', () => {
+    const insert3 = insert_decorations(test_armor2, [test_decoration3]);
+    expect(insert3.length).toBe(0);
+});
--- __test__/getter.test.js
+++ __test__/getter.test.js
@@ -4,3 +4,62 @@ const get_valid_decorations = getter.get_valid_decorations;
 const get_required_skills = getter.get_required_skills;
 const discard_outclassed_armors = getter.discard_outclassed_armors;
 
+it('gets required skills', () => {
+    const skill_names = ["Health -30"];
+    const skills = get_required_skills(skill_names);
+    expect(skills[0]["name"]).toBe("Health -30");
+    expect(skills[0]["skill-point"]).toBe("Health");
+    expect(skills[0]["points"]).toBe(-20);
+});
+
+it('gets valid armors according to provided skills', () => {
+    const skill_names = ["Health -30"];
+    const skills = get_required_skills(skill_names);
+    const valid_armors = get_valid_armors(skills);
+
+    expect(valid_armors.every(armor => {
+        return armor["skill-points"].some(skill_point => {
+            return skill_point["name"] === "Health" &&
+                    skill_point["points"] < 0;
+        });
+    }));
+});
+
+it ('gets valid decorations according to provided skills', () => {
+    const skill_names = ["Health -30"];
+    const skills = get_required_skills(skill_names);
+    const valid_decorations = get_valid_decorations(skills);
+
+    expect(valid_decorations.every(dec => {
+        return dec["skill-points"].some(skill_point => {
+            return skill_point["name"] === "Health" &&
+                    skill_point["points"] < 0;
+        });
+    }));
+});
+
+it ('discards all out-classed armors', () => {
+    // All filtered armors should at least 'tie' with the unfiltered armors
+    const skill_names = ["High Grade Earplug", "Evade +2", "Health -30"];
+    const skills = get_required_skills(skill_names);
+    const valid_armors = get_valid_armors(skills);
+    const get_optimal_armors = discard_outclassed_armors(valid_armors, skills);
+    const is_optimal = (optimal, competitors) => {
+        // Optimal armor must 'win' against every competitor
+        return competitors.every(competitor => {
+            return competitor["skill-points"].every(competitor_skill_point => {
+                return skills.every(skill => {
+                    // This skill is not included in the requirement, not interested
+                    if (!competitor_skill_point["name"] === skill["skill-point"]) return true;
+                    const optimal_skill_point = optimal["skill-points"]
+                        .find(s => s["name"] === competitor_skill_point["name"]);
+                    const points_for_optimal = optimal_skill_point ? optimal_skill_point["points"] : 0;
+                    const points_for_competitor = competitor_skill_point["points"];
+                    return skill["points"] > 0 ? points_for_optimal > points_for_competitor :
+                                            points_for_optimal < points_for_competitor;
+                });
+            }) || optimal["slots"] > competitor["slots"];
+        });
+    };
+    expect(get_optimal_armors.every(armor => is_optimal(armor, valid_armors)));
+});
